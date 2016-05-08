6669ac80a403bc9d0cf3379242b22a7fff7cb62d
Eko Kurniawan <setiawanegho@gmail.com>
2016-05-08 21:37:09 +0700

    bump 2.0.0 version

--- Nutrition.php
+++ Nutrition.php
@@ -17,7 +17,7 @@ final class Nutrition
      * Package Info
      */
     const PACKAGE = 'eghojansu/nutrition',
-          VERSION = '1.2.0';
+          VERSION = '2.0.0';
 
     /**
      * Base url
